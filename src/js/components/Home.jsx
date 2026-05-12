6ab22e91e5ee3bfb4d1c679d4904708972d83a16
Estefania Garza <lorenagh910@hotmail.com>
2026-05-12 02:43:36 +0000

    funciona

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,24 +1,62 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<div className="bg-dark p-1"></div>
-            <div className="d-inline-flex bg-dark m-2">
-				<div className="d-flex flex-column align-items-center gap-2 p-2">
-					<div className="col-4 bg-danger rounded-4 p-3"></div>
-					<div className="col-4 bg-warning rounded-4 p-3"></div>
-					<div className="col-4 bg-successh rounded-4 p-3"></div>
-				</div>
-			</div>
 
-		
-		</div>
-	);
+    const [color, setColor] = useState("green")
+
+    const colors = [
+        "red",
+        "yellow",
+        "green"
+    ]
+
+    function getColorBg (item) {
+        if (item=="red"){
+            return " bg-danger" // Espacio al inicio
+        }
+        if (item=="yellow"){
+            return " bg-warning" // Espacio al inicio
+        }
+        if (item=="green"){
+            return " bg-success" // Espacio al inicio
+        }
+    }
+
+    function isColorActive (item) {
+        if (item=="red" && item == color){
+            return " turn-on-red" // Espacio al inicio
+        }
+        if (item=="yellow" && item == color){
+            return " turn-on-yellow" // Espacio al inicio
+        }
+        if (item=="green" && item == color){
+            return " turn-on-green" // Espacio al inicio
+        }
+        return ""
+    }
+
+    return (
+        <div className="d-flex flex-column align-items-center">
+            <div className="bg-dark p-2" style={{width: '5px', height: '40px'}}></div>
+            <div className="d-inline-flex bg-dark">
+                <div className="d-flex flex-column align-items-center gap-2 p-2">
+                    {
+                        colors.map((item)=> {
+                            return (
+                                <div 
+                                    key={item} 
+                                    
+                                    className={"rounded-circle p-3" + getColorBg(item) + isColorActive(item)} 
+                                    style={{ width: "50px", height: "50px", cursor: "pointer" }}
+                                    onClick={() => setColor(item)}
+                                ></div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
